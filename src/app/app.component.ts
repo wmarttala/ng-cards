a10a3ccff366b3f910c1b4d7f490579f2068e237
Will Marttala <wmarttala123@gmail.com>
2022-08-29 11:20:56 -0500

    Completed the cards ng app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'mountainlover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking1222',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I did some biking today'
+    }
+  ]
 }
